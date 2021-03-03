9dfa6703a1667dffd229c74f2374adc2b9f3b49f
Gary Sun <garymajestic@gmail.com>
2021-03-02 17:09:59 -0800

    for switching work path according to user

--- clean_lib.py
+++ clean_lib.py
@@ -8,11 +8,10 @@ class CleanLib(deploy_job.DeployJob):
 
     def run(self):
         # list all files and dirs for lib dir
-        files = os.listdir(self.app_config["lib_path"])
+        files = os.listdir(self.app_config["lib64_path"])
         for fn in files:
             # for each file or dir, if not in the list, remove it
-            full_path = os.path.join(self.app_config["code_path"], self.app_config["app_name"],
-                                     self.app_config["lib_name"], fn)
+            full_path = os.path.join(self.app_config["lib64_path"], fn)
             if fn not in self.app_config["lib_file_dirs"]:
                 if os.path.exists(full_path):
                     if os.path.isfile(full_path):
@@ -23,5 +22,5 @@ class CleanLib(deploy_job.DeployJob):
 
     def __init__(self, app_config):
         super(CleanLib, self).__init__(app_config)
-        self.app_config["lib_path"] = os.path.join(self.app_config["code_path"], self.app_config["app_name"],
-                                                   self.app_config["lib_name"])
+        self.app_config["lib64_path"] = os.path.join(self.app_config["code_path"], self.app_config["app_name"],
+                                                     self.app_config["lib_path"], self.app_config["lib_name"])
--- set_secret.py
+++ set_secret.py
@@ -1,12 +1,52 @@
 import os
 import shutil
 import deploy_job
+import re
 
 
 class SetSecret(deploy_job.DeployJob):
     """ Copy the secret file to the specific direcotry """
 
+    @staticmethod
+    def set_path(line, name, value):
+        # find the work path. replace with the work path
+        m = re.search("^" + name + " *:", line)
+        if m is None:
+            return line
+        return name + ":" + value + "\n"
+
     def run(self):
+        self.set_work_path()
+        self.copy_secret()
+
+    def set_work_path(self):
+        """
+         change the work path in "templateDefaults.config" according to the installation path
+        """
+        work_path = [
+            {
+                "name": "rootscriptdir",
+                "value": os.path.join(self.app_config["code_path"], self.app_config["app_name"])
+            },
+            {
+                "name": "customizescriptdir",
+                "value": os.path.join(self.app_config["code_path"], self.app_config["app_name"],
+                                      self.app_config["script_specific"])
+            }
+        ]
+        lines = []
+        f = open(self.app_config["secret_src"], "r+")
+        text = f.readlines()
+        for line in text:
+            for pth in work_path:
+                line = self.set_path(line, pth["name"], pth["value"])
+            lines.append(line)
+        f.seek(0)
+        f.truncate()
+        f.writelines(lines)
+        f.close()
+
+    def copy_secret(self):
         # copy secret config file
         # this file contains sensitive setting values, keeping out from git repo
         shutil.copy(self.app_config["secret_src"], self.app_config["secret_dest"])
@@ -16,5 +56,6 @@ class SetSecret(deploy_job.DeployJob):
         super(SetSecret, self).__init__(app_config)
         self.app_config["secret_src"] = os.path.join(self.app_config["env_dir"], self.app_config["secret_name"])
         self.app_config["secret_dest"] = os.path.join(self.app_config["code_path"], self.app_config["app_name"],
+                                                      self.app_config["lib_path"],
                                                       self.app_config["secrets_dir_name"],
                                                       self.app_config["secret_name"])
